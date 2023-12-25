b89ec868d28029d7d4e60e447fa774dc84738a97
nazaradeh <aryoadeh@gmail.com>
2023-12-25 14:19:22 -0800

    Added vowels {ae} i u, and created an IPA decoder

---
+++ MS_SynthesizedVoice/Source/MS_SynthesizedVoice/CaseSensitiveMap.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MS_SYNTHESIZEDVOICE_API CaseSensitiveMap
+{
+public:
+	CaseSensitiveMap();
+	~CaseSensitiveMap();
+};
