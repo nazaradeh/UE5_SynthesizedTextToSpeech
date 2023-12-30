e0f95ce5e8202bcc327b78fa468bdc74edb3ed6c
nazaradeh <aryoadeh@gmail.com>
2023-12-29 17:36:38 -0800

    Removed old folder

--- MS_SynthesizedVoice/Source/MS_SynthesizedVoice/MS_SynthesizedVoice.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "MS_SynthesizedVoice.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MS_SynthesizedVoice, "MS_SynthesizedVoice" );
